3afaa91ca40df11da72b7647f6f84a7590156776
lantinglewis <lantinglewis@hotmail.com>
2023-02-13 21:13:22 +0100

    exercises on interfaces

---
+++ shape/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type triangle struct {
+	height float64
+	base   float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+func main() {
+	sq := square{sideLength: 5}
+	tr := triangle{height: 4.0, base: 2.0}
+
+	printArea(sq)
+	printArea(tr)
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
